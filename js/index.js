2b7c26c35b0608859d797aad726dd6a4532799f5
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-06-10 13:17:49 +0200

    CMW-189 Hide/show video while scrolling

--- js/index.js
+++ js/index.js
@@ -5,21 +5,48 @@ import "./preloader"
 import "./mainpage"
 import "../scss/index.scss"
 
-function updateBg() {
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+/*function updateBg() {
     if (!document.location.search.includes('noNoise'))//for better debuging
     {
         for (const element of document.querySelectorAll('html, .headerBg, #section-wrapper')) {
+            console.log("test")
             element.style.backgroundPosition = `${Math.round(Math.random() * 1000)}px ${Math.round(Math.random() * 1000)}px`
         }
     }
     requestAnimationFrame(updateBg)
 }
-updateBg();
-let lastScroll = 0;
+updateBg();*/
+
+let lastScrollTop = 0;
 addEventListener('scroll', e => {
 
-    document.documentElement.classList.toggle('isScrollUp', lastScroll > scrollY)
+    let video = document.querySelector('.video-container');
+    let galacticMission = document.querySelector('.galacticMission');
 
-    document.documentElement.style.setProperty('--scrollHeader', Math.min(1, scrollY / 157))
-    lastScroll = scrollY
-})
+
+    var isElInViewport = isInViewport(document.querySelector('.hideVideo'));
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+
+
+    if(isElInViewport) {
+        if (st > lastScrollTop){
+            video.style.opacity = "0";
+            galacticMission.style.display = "none";
+        } else {
+            video.style.opacity = "1";
+            galacticMission.style.display = "flex";
+        }
+    }
+
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false)
